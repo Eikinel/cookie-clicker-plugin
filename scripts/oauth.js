2f85df8e75605c33085211358c3955268252338e
Eikinel <valentin.pierrel@epitech.eu>
2019-12-26 18:54:37 +0100

    login: Loggin/Logout works

--- scripts/oauth.js
+++ scripts/oauth.js
@@ -1,9 +1,25 @@
 window.onload = function() {
-  var login = document.getElementById("login")
+  var logButton = document.querySelector("#log");
   
-  login.addEventListener('click', function() {
-    chrome.identity.getAuthToken({interactive: true}, function(token) {
-      console.log(token);
-    });
+  chrome.identity.getAuthToken({interactive: false}, function(token) {    
+    localStorage.setItem('token', token);
+
+    if (token) {
+      logButton.innerHTML = "Sign out";
+      logButton.addEventListener('click', function() {
+        window.fetch(`https://accounts.google.com/o/oauth2/revoke?token=${token}`);
+        chrome.identity.removeCachedAuthToken({ token: localStorage.getItem('token') }, function() {
+          localStorage.removeItem('token');
+          console.log('Logged out');
+        });
+      });
+    } else {
+      logButton.innerHTML = "Sign in";
+      logButton.addEventListener('click', function() {
+        chrome.identity.getAuthToken({interactive: true}, function(token) {
+          localStorage.setItem('token', token);
+        });
+      });
+    }
   });
 };
