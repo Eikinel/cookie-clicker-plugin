27b02517f82f3bcd5df3ad15bb075c6d2d4d2c10
Eikinel <valentin.pierrel@epitech.eu>
2019-12-26 19:56:00 +0100

    wip: Stylising login using Cookie Clicker's assets

--- scripts/oauth.js
+++ scripts/oauth.js
@@ -1,21 +1,24 @@
 window.onload = function() {
+  var authTitle = document.querySelector("#auth-title");
   var logButton = document.querySelector("#log");
   
   chrome.identity.getAuthToken({interactive: false}, function(token) {    
     localStorage.setItem('token', token);
 
     if (token) {
-      logButton.innerHTML = "Sign out";
+      authTitle.classList.add("hide");
+      logButton.value = "Sign out";
       logButton.addEventListener('click', function() {
         window.fetch(`https://accounts.google.com/o/oauth2/revoke?token=${token}`);
         chrome.identity.removeCachedAuthToken({ token: localStorage.getItem('token') }, function() {
           localStorage.removeItem('token');
           console.log('Logged out');
+          window.location.reload();
         });
-        window.location.reload();
       });
     } else {
-      logButton.innerHTML = "Sign in";
+      authTitle.classList.remove("hide");
+      logButton.value = "Sign in";
       logButton.addEventListener('click', function() {
         chrome.identity.getAuthToken({interactive: true}, function(token) {
           localStorage.setItem('token', token);
