8507f69a49ddb239f9f5306c270e2b82191f1908
PIERREL Valentin <valentin.pierrel@neo9.fr>
2019-12-27 09:53:59 +0100

    login: Switch to menu if already logged in

--- scripts/oauth.js
+++ scripts/oauth.js
@@ -1,30 +1,37 @@
 window.onload = function() {
-  var authTitle = document.querySelector("#auth-title");
   var logButton = document.querySelector("#log");
   
-  chrome.identity.getAuthToken({interactive: false}, function(token) {    
-    localStorage.setItem('token', token);
-
-    if (token) {
-      authTitle.classList.add("hide");
-      logButton.value = "Sign out";
+  chrome.identity.getAuthToken({interactive: false}, function(token) {
+    if (!token) {
+      /*logButton.value = "Sign out";
       logButton.addEventListener('click', function() {
-        window.fetch(`https://accounts.google.com/o/oauth2/revoke?token=${token}`);
-        chrome.identity.removeCachedAuthToken({ token: localStorage.getItem('token') }, function() {
-          localStorage.removeItem('token');
-          console.log('Logged out');
-          setTimeout(() => window.location.reload(), 2000);
-        });
+        window
+          .fetch(`https://accounts.google.com/o/oauth2/revoke?token=${token}`)
+          .then((response) => {
+            chrome.identity.removeCachedAuthToken({ token: localStorage.getItem('token') }, function() {
+              localStorage.removeItem('token');
+            });
+            console.log('Logged out');
+            chrome.pageAction.setPopup({tabId: 0, popup: "menu.html"});
+
+            return response;
+          });
       });
-    } else {
-      authTitle.classList.remove("hide");
+    } else {*/
       logButton.value = "Sign in";
       logButton.addEventListener('click', function() {
         chrome.identity.getAuthToken({interactive: true}, function(token) {
           localStorage.setItem('token', token);
-          window.location.reload();
+          console.log('Logged in');
+          chrome.browserAction.setPopup({popup: "html/login.html"});
+          window.location.href = "login.html";
         });
       });
+    } else {
+      console.log("Token found");
+      localStorage.setItem('token', token);
+      chrome.browserAction.setPopup({popup: "html/menu.html"});
+      window.location.href = "menu.html";
     }
   });
 };
