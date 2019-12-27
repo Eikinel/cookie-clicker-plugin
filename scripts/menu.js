886c907bbb1e30240a2785052beb350ba08f5f46
PIERREL Valentin <valentin.pierrel@neo9.fr>
2019-12-27 11:31:41 +0100

    menu: Get user data

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,21 +1,19 @@
 window.onload = async function() {
     const logoutButton = document.querySelector("#logout");
     const p = new Promise((resolve, reject) => {
-        chrome.storage.local.get(["token"], (items) => resolve(items.token));
+        chrome.storage.local.get(["token", "userInfo"], (items) => resolve(items));
     });
-    const token = await p;
+    const storage = await p;
+    this.console.log(storage);
 
-    console.log(token);
     logoutButton.addEventListener('click', function() {
         window
-            .fetch(`https://accounts.google.com/o/oauth2/revoke?token=${token}`)
+            .fetch(`https://accounts.google.com/o/oauth2/revoke?token=${storage.token}`)
             .then((response) => {
-                chrome.identity.removeCachedAuthToken({token: token}, function() {
-                     chrome.storage.local.remove("token");
-                });
+                chrome.identity.removeCachedAuthToken({token: storage.token});
                     
                 console.log('Logged out');
-                chrome.storage.local.remove("token");
+                chrome.storage.local = {};
                 chrome.browserAction.setPopup({popup: "html/login.html"});
                 window.location.href = "login.html";
 
@@ -23,11 +21,18 @@ window.onload = async function() {
             });
     });
 
-    window
-        .fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${token}`)
-        .then((response) => response.json())
-        .then((data) => {
-            this.console.log(data);
-            document.querySelector("#name").innerHTML = data.given_name;
-        })
+    if (!storage.userInfo) {
+        await window
+            .fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${storage.token}`)
+            .then((response) => response.json())
+            .then((data) => {
+                this.console.log("Stock user data");
+                chrome.storage.local.set({ "userInfo": data }, () => {
+                    chrome.storage.local.get(["token", "userInfo"], (r) => this.console.log(r));
+                });
+                document.querySelector("#name").innerHTML = data.given_name;
+            })
+    } else {
+        document.querySelector("#name").innerHTML = storage.userInfo.given_name;
+    }
 }
