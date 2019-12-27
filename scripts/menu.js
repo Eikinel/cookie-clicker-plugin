bac8dc043d9b697dea8351dd9f222dfe22ab4abc
PIERREL Valentin <valentin.pierrel@neo9.fr>
2019-12-27 15:45:58 +0100

    menu: Save button event listener matches Cookie Clicker's URL

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,12 +1,23 @@
 window.onload = async function() {
     const logoutButton = document.querySelector("#logout");
-    const p = new Promise((resolve, reject) => {
+    const createSaveButton = document.querySelector("#add-save");
+    const pStorage = new Promise((resolve, reject) => {
         chrome.storage.local.get(["token", "userInfo"], (items) => resolve(items));
     });
-    const storage = await p;
-    this.console.log(storage);
+    const storage = await pStorage;
 
-    logoutButton.addEventListener('click', function() {
+    // Get user info
+    if (!storage.userInfo) {
+        await window
+            .fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${storage.token}`)
+            .then((response) => response.json())
+            .then((data) => chrome.storage.local.set({ "userInfo": data }));
+    }
+    document.querySelector("#name").innerHTML = storage.userInfo.given_name;
+    document.querySelector("#avatar").src = storage.userInfo.picture;
+
+    // Buttons listeners
+    logoutButton.addEventListener('click', () => {
         window
             .fetch(`https://accounts.google.com/o/oauth2/revoke?token=${storage.token}`)
             .then((response) => {
@@ -21,14 +32,25 @@ window.onload = async function() {
             });
     });
 
-    if (!storage.userInfo) {
-        await window
-            .fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${storage.token}`)
-            .then((response) => response.json())
-            .then((data) => chrome.storage.local.set({ "userInfo": data }));
-    }
-
-    this.console.log(storage.userInfo);
-    document.querySelector("#name").innerHTML = storage.userInfo.given_name;
-    document.querySelector("#avatar").src = storage.userInfo.picture;
+    createSaveButton.addEventListener('click', () => {
+        new Promise((resolve, reject) => chrome.tabs.query({ url: "*://orteil.dashnet.org/cookieclicker/" }, (tab) => {
+            console.log(tab);
+            resolve(tab);
+        }))
+        .then((tab) => {
+            return new Promise((resolve, reject) => {
+                if (tab && tab[0]) {
+                    chrome.tabs.executeScript(tab[0].id, { code: "localStorage['CookieClickerGame']" }, (gameHash) => resolve(gameHash));
+                } else {
+                    reject("No opened tab matching Cookie Clicker URL");
+                }
+            });
+        })
+        .then((gameHash) => {
+            console.log(gameHash);
+            if (gameHash) {
+                // Save to user's drive
+            }
+        })        
+    });
 }
