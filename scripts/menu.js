9d957e505f8b183d29a8394b4a3f1ed6e3ed7c93
Eikinel <valentin.pierrel@epitech.eu>
2019-12-29 16:41:42 +0100

    wip: CRUD wrapper

--- scripts/menu.js
+++ scripts/menu.js
@@ -19,9 +19,7 @@ window.onload = async function() {
         getAuthToken().then((token) => logout(token));
     });
 
-    refreshListButton.addEventListener('click', () => {
-        getAuthToken().then((token) => this.listSaves(token));
-    });
+    refreshListButton.addEventListener('click', listSaves());
 
     createSaveButton.addEventListener('click', () => {
         new Promise((resolve) => {
@@ -36,66 +34,93 @@ window.onload = async function() {
         })
         .then((gameHash) => {
             console.log(gameHash);
-            if (gameHash) {
-                // Save to user's drive
-            }
+            gameHash ? createSave() : 0;
         })        
     });
 
     // List user's saves from Drive
-    getAuthToken().then((token) => this.listSaves(token));
+    listSaves();
 }
 
 
 // CRUD
-async function createSave(token) {
-    
+async function createSave() {
+    crudRequestWrapper()
+    .then((wrapper) => {
+        console.log(wrapper);
+    });
 }
 
-async function listSaves(token) {
-    const folderId = await getSaveFolderId(token)
-    
-    console.log(folderId);
-    return fetch(`https://www.googleapis.com/drive/v3/files?key=${API_KEY}` +
-    `&pageSize=5&q='${folderId}' in parents and mimeType!='application/vnd.google-apps.folder'`, {
-        headers: getHeaders(token)
-    })
-    .then((res) => res.json())
-    .then((filesJson) => {
-        const listDiv = document.querySelector("#save-list");
+async function listSaves() {
+    crudRequestWrapper()
+    .then((wrapper) => {
+        fetch(`https://www.googleapis.com/drive/v3/files?key=${API_KEY}` +
+        `&pageSize=5&q='${wrapper.folderId}' in parents and mimeType!='application/vnd.google-apps.folder'`, {
+            headers: getHeaders(wrapper.token)
+        })
+        .then((res) => res.json())
+        .then((filesJson) => {
+            const listDiv = document.querySelector("#save-list");
 
-        // Clear div
-        listDiv.innerHTML = "";
+            // Clear div
+            listDiv.innerHTML = "";
 
-        // Populate div
-        for (const file of filesJson.files) {
-            listDiv.innerHTML +=
-            `<div class="d-flex listing justify-content-flex-end flex-wrap w-100">
-                <span class="flex-grow-1">${file.name}</span>
-                <div class="d-flex listing">
-                    <a id="save-${file.id}" class="option">Save</a>
-                    <a id="use-${file.id}" class="option">Use</a>
-                    <a id="delete-${file.id}" class="option warning">Delete</a>
-                </div>
-            </div>`
+            // Populate div
+            for (const file of filesJson.files) {
+                listDiv.innerHTML +=
+                `<div class="d-flex listing justify-content-flex-end flex-wrap w-100">
+                    <span class="flex-grow-1">${file.name}</span>
+                    <div class="d-flex listing">
+                        <a id="save-${file.id}" class="option">Save</a>
+                        <a id="use-${file.id}" class="option">Use</a>
+                        <a id="delete-${file.id}" class="option warning">Delete</a>
+                    </div>
+                </div>`
 
-            document.querySelector(`#save-${file.id}`).addEventListener('click', () => {
-                console.log(`Saving to ${file.id}`);
-            });
+                document.querySelector(`#save-${file.id}`).addEventListener('click', () => {
+                    console.log(`Saving to ${file.id}`);
+                });
 
-            document.querySelector(`#use-${file.id}`).addEventListener('click', () => {
-                console.log(`Using ${file.id}`);
-            });
+                document.querySelector(`#use-${file.id}`).addEventListener('click', () => {
+                    console.log(`Using ${file.id}`);
+                });
 
-            document.querySelector(`#delete-${file.id}`).addEventListener('click', () => {
-                console.log(`Deleting ${file.id}`);
-            });
-        }
-    })
+                document.querySelector(`#delete-${file.id}`).addEventListener('click', () => {
+                    console.log(`Deleting ${file.id}`);
+                });
+            }
+        });
+    });
+}
+
+async function updateSave() {
+    crudRequestWrapper()
+    .then((wrapper) => {
+        console.log(wrapper);
+    });    
+}
+
+async function deleteSave() {
+    crudRequestWrapper()
+    .then((wrapper) => {
+        console.log(wrapper);
+    });    
 }
 
 
 // Methods
+async function crudRequestWrapper() {
+    const token = await getAuthToken();
+    
+    return getSaveFolderId(token)
+    .then((folderId) => {
+        return {
+            token: token,
+            folderId: folderId
+        }
+    });
+}
+
 async function getSaveFolderId(token) {
     return fetch(`https://www.googleapis.com/drive/v3/files?key=${API_KEY}` +
     `&q=mimeType='application/vnd.google-apps.folder' and name='${SAVE_FOLDER}'`, {
